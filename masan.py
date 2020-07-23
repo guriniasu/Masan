f72e132781f1f1591a5f2bff6a2557aeeaf68c5e
guriniasu <48523975+guriniasu@users.noreply.github.com>
2020-07-23 19:54:06 +0900

    Initial commit

---
+++ masan.py
@@ -0,0 +1,9 @@
+print("「マサン」を出力する回数を入力してください（半角数字）")
+n=input()
+with open("masan.txt",mode="w",encoding="shift_jis") as txt:
+    print("マサンを"+n+"回出力します\nつまり"+str(int(n)*3)+"文字分！")
+    for i in range(int(n)):
+        txt.write("マサン")
+    print("同じ階層に「masan.txt」ができたよ")
+    input()
+    
